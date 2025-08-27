38b6c6c736dd3fcf1c237d2930dc5e9160246350
tabortoa <815141681@qq.com>
2025-08-27 15:48:16 +0800

    取消Gin框架关于信任所有代理的警告信息

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"taskgo/internal/database"
 	"taskgo/internal/router"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode) //正式发布阶段使用
 	database.ConnectDatabase()
 	r := router.SetupRouter(database.DB)
 	r.Run(":8080")
--- internal/router/router.go
+++ internal/router/router.go
@@ -10,6 +10,7 @@ import (
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+	r.SetTrustedProxies(nil) //取消Gin框架关于信任所有代理的警告信息
 
 	r.Static("/static", "./web/static")
 	r.LoadHTMLGlob("web/templates/*")
@@ -36,7 +37,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 			authed.PUT("/tasks/:id", handlers.UpdateTask(db))
 			authed.DELETE("/tasks/:id", handlers.DeleteTask(db))
 			authed.POST("/tasks/update-tags", handlers.UpdateTagForAllTasks(db))
-			
+
 			// 评论相关路由
 			authed.GET("/tasks/:id/comments", handlers.GetTaskComments(db))
 			authed.POST("/tasks/:id/comments", handlers.CreateTaskComment(db))
